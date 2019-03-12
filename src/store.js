67c06d89cc3ca3880ad217ed05c536316236218c
Daniel Yoo <yoo.d93@gmail.com>
2019-03-12 13:53:41 -0400

    added try catch for pushing data

--- src/store.js
+++ src/store.js
@@ -19,20 +19,22 @@ const store = {
 
 const pushData = async (data) => {
     for (let key in data.data) {
-        const docRef = db.collection(data.year)
-            .doc(data.month)
-            .collection(data.date)
-            .doc(key);
-    
-        const snapshot = await docRef.get()
-        const result = snapshot.data() || {time: 0};
-    
-        docRef.set({
-            time: (result.time + data.data[key])
-        });   
+        try {
+            const docRef = db.collection(data.year)
+                .doc(data.month)
+                .collection(data.date)
+                .doc(key);
+        
+            const snapshot = await docRef.get()
+            const result = snapshot.data() || {time: 0};
+        
+            docRef.set({
+                time: (result.time + data.data[key])
+            });   
+        } catch(e) {
+            console.error("There was an error with trying to push data : ", data);
+        }
     }
 }
 
 export default store;
-
-// "start": "nodemon --exec babel-node src/index.js | npm start -prefix web",
